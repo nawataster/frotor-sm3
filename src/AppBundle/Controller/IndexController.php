79f6dc03e93a0bd8fc381836463bc1801f53e3dd
ckolenchenko <ckolenchenko@yukon.cv.ua>
2018-08-10 17:00:55 +0300

    Logger callin simplify.

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -3,41 +3,20 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Psr\Log\LoggerInterface;
+// use Symfony\Component\HttpFoundation\Response;
+// use Psr\Log\LoggerInterface;
 
 class IndexController extends Controller{
-
-	private $lg;
-
-	public function __construct( LoggerInterface $logger ){
-		$this->lg	= $logger;
-	}
-//______________________________________________________________________________
 
 	public function indexAction( Request $request, $num ) {
 
 		$number = $num;
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
-
+		return $this->render('pages/index.html.twig', [
+            'number' => $number,
+			'base_dir'	=> 'Dummy-base-dir'
+        ]);
 	}
 //______________________________________________________________________________
 
 }//class end
-
-//Commit before amend.
-
-
-// 	$faucet = new \App\Model\Faucet();
-//     $faucet->setUrl(rand(1, 11000).'-test/url/tst.tst');
-//     $faucet->setQuery(rand(1, 11000).'-test/query/tst.qqq');
-//     $faucet->setInfo(rand(1, 11000).'-test/INFO/tst.inf');
-// //     $faucet->setDuration(10);
-//     $faucet->setUntil( new \DateTime('2018-10-01 00:00:01') );
-
-//     $entityManager = $app['orm.em'];
-//     $entityManager->persist( $faucet );
-//     $entityManager->flush();
