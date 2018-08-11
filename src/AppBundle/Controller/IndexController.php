089118af64624e64a23a83f981e8dd89e3ebd9ef
Constantine <nawataster@gmail.com>
2018-08-11 14:08:35 +0300

    FaucetService init and configer.

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -3,18 +3,20 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Service\FaucetService;
 // use Symfony\Component\HttpFoundation\Response;
 // use Psr\Log\LoggerInterface;
 
 class IndexController extends Controller{
 
-	public function indexAction( Request $request, $num ) {
+	public function indexAction( Request $request ) {
 
-		$number = $num;
+		$fsrv	= $this->container->get(FaucetService::class);
+
+		$faucet	= $fsrv->getFirstReadyFaucet();
+
 
 		return $this->render('pages/index.html.twig', [
-            'number' => $number,
-			'base_dir'	=> 'Dummy-base-dir'
         ]);
 	}
 //______________________________________________________________________________
--- src/AppBundle/Service/FaucetService.php
+++ src/AppBundle/Service/FaucetService.php
@@ -1,11 +1,15 @@
 <?php
+namespace AppBundle\Service;
+
 class FaucetService{
+
+
+	public function __construct(){}
+
 	public function getFirstReadyFaucet(){
-
-
-		return [
+		return (object)[
 			'id'	=> 500,
-			''
+			'info'	=> 'Dummy faucet name'
 		];
 
 	}
